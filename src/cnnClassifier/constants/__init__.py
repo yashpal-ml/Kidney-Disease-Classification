f128832586f459db08fb82168c3b613b9b1f965d
Yashpal Sharma Nagaraja Rao <y.sharma.nagarajarao@gmail.com>
2024-12-17 23:49:23 -0500

    defined entity, config and components for data ingestion

--- src/cnnClassifier/constants/__init__.py
+++ src/cnnClassifier/constants/__init__.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 CONFIG_FILE_PATH = Path('config/config.yaml')
+# CONFIG_FILE_PATH = Path('C:\Users\yashp\Documents\Kidney-Disease-Classification\src\config\config.yaml')
 PARAMS_FILE_PATH = Path('params.yaml')
 # print(CONFIG_FILE_PATH)
 # artifacts_root = ""
