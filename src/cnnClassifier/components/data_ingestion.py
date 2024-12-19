5b0d5ac4b80f596d1eae0356237869ede869a1ed
Yashpal Sharma Nagaraja Rao <y.sharma.nagarajarao@gmail.com>
2024-12-19 17:24:28 -0500

    Prepare Base Model changes completed

--- src/cnnClassifier/components/data_ingestion.py
+++ src/cnnClassifier/components/data_ingestion.py
@@ -3,7 +3,7 @@ import zipfile
 import gdown
 from cnnClassifier import logger
 from cnnClassifier.utils.common import get_size
-from cnnClassifier.config.configuration import DataIngestionConfig
+from cnnClassifier.entity.config_entity import DataIngestionConfig
 
 class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -8,3 +8,17 @@ class DataIngestionConfig:
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+    
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_include_top: bool
+    # params_batch_size: int
+    # params_epochs: int
+    params_classes: int
+    params_weights: str
+    params_learning_rate: float
