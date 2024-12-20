3d2038cac1b2b9293bfaaade579fb7ea48421948
Yashpal Sharma Nagaraja Rao <y.sharma.nagarajarao@gmail.com>
2024-12-19 19:17:02 -0500

    Model Training code added

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -22,3 +22,15 @@ class PrepareBaseModelConfig:
     params_classes: int
     params_weights: str
     params_learning_rate: float
+
+
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
